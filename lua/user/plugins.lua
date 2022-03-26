76c454e1ccc14a9fe1eeae1b50e9ec0d30207f67
Francisco De La Cruz <helloquico@gmail.com>
2022-03-26 18:18:35 +0100

    add telescope-ui-select

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -130,7 +130,8 @@ return packer.startup(function(use)
   -- Telescope
   use "nvim-telescope/telescope.nvim"
   use "tom-anders/telescope-vim-bookmarks.nvim"
-  use "nvim-telescope/telescope-media-files.nvim" 
+  use "nvim-telescope/telescope-media-files.nvim"
+  use "nvim-telescope/telescope-ui-select.nvim"
 
   -- Treesitter
   use {
