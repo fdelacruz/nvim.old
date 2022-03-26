0d8abad77792aaf559105d1ceab55f9d54538bcb
Francisco De La Cruz <helloquico@gmail.com>
2022-03-26 07:47:07 +0100

    lsp updates

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -19,6 +19,11 @@ lsp_installer.on_server_ready(function(server)
 	 if server.name == "sumneko_lua" then
 	 	local sumneko_opts = require("user.lsp.settings.sumneko_lua")
 	 	opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+    end
+
+   if server.name == "pyright" then
+	 	local pyright_opts = require("user.lsp.settings.pyright")
+	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 
    if server.name == "rust_analyzer" then
