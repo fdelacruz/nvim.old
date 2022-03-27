4cb9450a47bb90d7e4ff7523d9b83ada40a35668
Francisco De La Cruz <helloquico@gmail.com>
2022-03-27 07:56:58 +0200

    use emmet_ls

--- lua/user/lsp/lsp-installer.lua
+++ lua/user/lsp/lsp-installer.lua
@@ -26,6 +26,11 @@ lsp_installer.on_server_ready(function(server)
 	 	opts = vim.tbl_deep_extend("force", pyright_opts, opts)
 	 end
 
+   if server.name == "emmet_ls" then
+    local emmet_ls_opts = require "user.lsp.settings.emmet_ls"
+    opts = vim.tbl_deep_extend("force", emmet_ls_opts, opts)
+   end
+
    if server.name == "rust_analyzer" then
     local rustopts = {
             tools = {
