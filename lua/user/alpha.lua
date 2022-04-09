69fa2067b384364a24a794793236c1e4d43a8b9e
Francisco De La Cruz <helloquico@gmail.com>
2022-04-09 00:02:16 -0400

    add Telescope File Browser to menus

--- lua/user/alpha.lua
+++ lua/user/alpha.lua
@@ -22,7 +22,8 @@ dashboard.section.buttons.val = {
 		icons.git.Repo .. " Find project",
 		":lua require('telescope').extensions.projects.projects()<CR>"
 	),
-	dashboard.button("r", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
+	dashboard.button("r", icons.documents.Folder .. " Browser", ":Telescope file_browser <CR>"),
+  dashboard.button("o", icons.ui.History .. " Recent files", ":Telescope oldfiles <CR>"),
 	dashboard.button("t", icons.ui.List .. " Find text", ":Telescope live_grep <CR>"),
 	dashboard.button("s", icons.ui.SignIn .. " Find Session", ":Telescope sessions save_current=false <CR>"),
 	dashboard.button("c", icons.ui.Gear .. " Config", ":e ~/.config/nvim/init.lua <CR>"),
