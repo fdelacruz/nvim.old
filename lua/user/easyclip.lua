cc300dbdb13204059df9664ee21755f0156d63fc
Francisco De La Cruz <helloquico@gmail.com>
2022-04-10 10:48:19 -0400

    update easyclip

--- lua/user/easyclip.lua
+++ lua/user/easyclip.lua
@@ -1,7 +1,8 @@
 vim.g.EasyClipUseCutDefaults = 0
 
 vim.cmd [[
-  nmap x <Plug>MoveMotionPlug
-  xmap x <Plug>MoveMotionXPlug
+  " unset for neovim
+  " nmap x <Plug>MoveMotionPlug
+  " xmap x <Plug>MoveMotionXPlug
   nmap xx <Plug>MoveMotionLinePlug
 ]]
