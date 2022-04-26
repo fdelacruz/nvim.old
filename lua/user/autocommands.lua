b0139bfaa03f9450021ea352a7dca3bc98675123
Francisco De La Cruz <helloquico@gmail.com>
2022-04-25 21:29:54 -0400

    properly setup quick-scope new highlighs

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -14,6 +14,12 @@ vim.cmd [[
 
   augroup end
 
+  augroup qs_colors
+    autocmd!
+    autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
+    autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+  augroup end
+
   augroup _git
     autocmd!
     autocmd FileType gitcommit setlocal wrap
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -23,14 +23,6 @@ vim.cmd [[
   augroup end
 ]]
 
-vim.cmd [[
-  augroup qs_colors
-    autocmd!
-    autocmd ColorScheme * highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
-    autocmd ColorScheme * highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
-  augroup END
-]]
-
 -- Use a protected call so we don't error out on first use
 local status_ok, packer = pcall(require, "packer")
 if not status_ok then
