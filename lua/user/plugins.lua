73c14fd52d88eb1000b7638afc61a8f4afd01a58
Francisco De La Cruz <helloquico@gmail.com>
2022-05-09 01:00:12 -0400

    add tmux.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -82,6 +82,29 @@ return packer.startup(function(use)
   use "SmiteshP/nvim-gps"
   use "matbme/JABS.nvim"
 
+  -- tmux
+  use { "aserowy/tmux.nvim",
+        config = function()
+        require("tmux").setup({
+            -- overwrite default configuration
+            -- here, e.g. to enable default bindings
+            copy_sync = {
+                -- enables copy sync and overwrites all register actions to
+                -- sync registers *, +, unnamed, and 0 till 9 from tmux in advance
+                enable = true,
+            },
+            navigation = {
+                -- enables default keybindings (C-hjkl) for normal mode
+                enable_default_keybindings = true,
+            },
+            resize = {
+                -- enables default keybindings (A-hjkl) for normal mode
+                enable_default_keybindings = true,
+            }
+        })
+    end
+  }
+
   -- Colorschemes
   use "rebelot/kanagawa.nvim"
 
