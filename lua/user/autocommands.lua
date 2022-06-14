0caddd2d85ce849092a7729dbcba3aeae5bafc43
Francisco De La Cruz <helloquico@gmail.com>
2022-06-14 18:49:29 -0400

    update autocommands

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -1,67 +1,70 @@
 vim.cmd [[
   augroup _general_settings
     autocmd!
-    autocmd FileType qf,help,man,lspinfo, nnoremap <silent> <buffer> q :close<CR> 
-    autocmd BufWinEnter * :set formatoptions-=cro
+    autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR> 
     autocmd FileType qf set nobuflisted
-    autocmd CmdWinEnter * quit
-
-    autocmd BufReadPost *
-          \ if line("'\"") > 0 && line("'\"") <= line("$") |
-          \   exe "normal g`\"" |
-          \ endif
-
   augroup end
-
-  augroup _git
-    autocmd!
-    autocmd FileType gitcommit setlocal wrap
-    autocmd FileType gitcommit setlocal spell
-  augroup end
-
-  augroup _markdown
-    autocmd!
-    autocmd FileType markdown setlocal wrap
-    autocmd FileType markdown setlocal spell
-  augroup end
-
-  augroup _auto_resize
-    autocmd!
-    autocmd VimResized * tabdo wincmd = 
-  augroup end
-
-  augroup _alpha
-    autocmd!
-    autocmd User AlphaReady set showtabline=0 | autocmd BufUnload <buffer> set showtabline=2
-  augroup end
-
-  augroup illuminate_augroup
-    autocmd!
-    autocmd VimEnter * hi link illuminatedWord LspReferenceText
-  augroup end
-
   autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
-
 ]]
 
-vim.api.nvim_create_autocmd({ "Colorscheme" }, {
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = { "gitcommit", "markdown" },
   callback = function()
     vim.cmd [[
-      highlight QuickScopePrimary guifg='#afff5f' gui=underline ctermfg=155 cterm=underline
-      highlight QuickScopeSecondary guifg='#5fffff' gui=underline ctermfg=81 cterm=underline
+    setlocal wrap
+    setlocal spell
     ]]
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-  pattern = { "*.rs" },
-  callback = function ()
-    vim.lsp.codelens.refresh()
-  end
+-- TODO why this no work?
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--   callback = 
+--     "++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
+-- })
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+  callback = function()
+    vim.cmd "tabdo wincmd ="
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+  callback = function()
+    vim.cmd "quit"
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+  callback = function()
+    vim.cmd "set formatoptions-=cro"
+  end,
 })
 
 vim.api.nvim_create_autocmd({ "TextYankPost" }, {
-  callback = function ()
-    vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 })
-  end
+  callback = function()
+    vim.highlight.on_yank { higroup = "Visual", timeout = 200 }
+  end,
 })
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+  pattern = { "*.java" },
+  callback = function()
+    vim.lsp.codelens.refresh()
+  end,
+})
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    vim.cmd "hi link illuminatedWord LspReferenceText"
+  end,
+})
+
+-- autocmd BufLeave * if (!exists('b:caret')) | let b:caret = winsaveview() | endif
+-- autocmd BufEnter * if (exists('b:caret')) | call winrestview(b:caret) | endif
+
+-- Autoformat
+-- augroup _lsp
+--   autocmd!
+--   autocmd BufWritePre * lua vim.lsp.buf.formatting()
+-- augroup end
