6bdd9f363d499fc980c9d1c70d137a28e7597c12
Francisco De La Cruz <helloquico@gmail.com>
2022-06-15 07:40:13 -0400

    formatting init.lua

--- init.lua
+++ init.lua
@@ -1,45 +1,45 @@
-require("user.options")
-require("user.keymaps")
-require("user.plugins")
-require("user.autocommands")
-require("user.cmp")
-require("user.lsp")
-require("user.telescope")
-require("user.treesitter")
-require("user.autopairs")
-require("user.comment")
-require("user.gitsigns")
-require("user.nvim-tree")
-require("user.lualine")
-require("user.toggleterm")
-require("user.project")
-require("user.impatient")
-require("user.indentline")
-require("user.alpha")
-require("user.whichkey")
-require("user.quickscope")
-require("user.colorscheme")
-require("user.bufferline")
-require("user.hop")
-require("user.matchup")
-require("user.numb")
-require("user.colorizer")
-require("user.spectre")
-require("user.zen-mode")
-require("user.neoscroll")
-require("user.todo-comments")
-require("user.bookmark")
-require("user.renamer")
-require("user.symbol-outline")
-require("user.gist")
-require("user.gitlinker")
-require("user.session-manager")
-require("user.surround")
-require("user.notify")
-require("user.ts-context")
-require("user.registers")
-require("user.gps")
-require("user.illuminate")
-require("user.lir")
-require("user.jabs")
-require("user.winbar")
+require "user.options"
+require "user.keymaps"
+require "user.plugins"
+require "user.autocommands"
+require "user.cmp"
+require "user.lsp"
+require "user.telescope"
+require "user.treesitter"
+require "user.autopairs"
+require "user.comment"
+require "user.gitsigns"
+require "user.nvim-tree"
+require "user.lualine"
+require "user.toggleterm"
+require "user.project"
+require "user.impatient"
+require "user.indentline"
+require "user.alpha"
+require "user.whichkey"
+require "user.quickscope"
+require "user.colorscheme"
+require "user.bufferline"
+require "user.hop"
+require "user.matchup"
+require "user.numb"
+require "user.colorizer"
+require "user.spectre"
+require "user.zen-mode"
+require "user.neoscroll"
+require "user.todo-comments"
+require "user.bookmark"
+require "user.renamer"
+require "user.symbol-outline"
+require "user.gist"
+require "user.gitlinker"
+require "user.session-manager"
+require "user.surround"
+require "user.notify"
+require "user.ts-context"
+require "user.registers"
+require "user.gps"
+require "user.illuminate"
+require "user.lir"
+require "user.jabs"
+require "user.winbar"
