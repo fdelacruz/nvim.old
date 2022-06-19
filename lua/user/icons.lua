d8243db7e45fed95dab13f3ace6c5dfacc166b5a
Francisco De La Cruz <helloquico@gmail.com>
2022-06-19 11:21:37 -0400

    update icons

--- lua/user/icons.lua
+++ lua/user/icons.lua
@@ -2,7 +2,8 @@
 -- go to the above and then enter <c-v>u<unicode> and the symbold should appear
 -- or go here and upload the font file: https://mathew-kurian.github.io/CharacterMap/
 -- find more here: https://www.nerdfonts.com/cheat-sheet
-if vim.fn.has "mac" == 1 then
+vim.g.use_nerd_icons = false
+if (vim.fn.has "mac" == 1 or vim.g.use_nerd_icons) then
   return {
     kind = {
       Text = "",
@@ -80,6 +81,8 @@ if vim.fn.has "mac" == 1 then
       Package = "",
       List = "",
       SignIn = "",
+      SignOut = "",
+      NoteBook = " ",
       Check = "",
       Fire = "",
       Note = "",
@@ -179,6 +182,8 @@ else
       Package = " ",
       List = " ",
       SignIn = " ",
+      SignOut = "",
+      NoteBook = " ",
       Check = " ",
       Fire = " ",
       Note = " ",
