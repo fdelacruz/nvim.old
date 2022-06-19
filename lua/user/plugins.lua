57780884370a71c050f81c153f8b134c75e7dcc9
Francisco De La Cruz <helloquico@gmail.com>
2022-06-19 15:04:02 -0400

    pin lsp_signature

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -158,7 +158,10 @@ return packer.startup(function(use)
   use "jose-elias-alvarez/null-ls.nvim" -- for formatters and linters
   use "filipdutescu/renamer.nvim"
   use "simrat39/symbols-outline.nvim"
-  use "ray-x/lsp_signature.nvim"
+  use {
+    "ray-x/lsp_signature.nvim",
+    commit = "4852d99f9511d090745d3cc1f09a75772b9e07e9"
+  }
   use {
     "folke/trouble.nvim",
     cmd = "TroubleToggle",
