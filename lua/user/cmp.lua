767f173886808500c6187c73a9e3633363fa8a33
Francisco De La Cruz <helloquico@gmail.com>
2022-06-20 19:03:11 -0400

    cmp cleanup

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -72,24 +72,13 @@ cmp.setup {
     fields = { "kind", "abbr", "menu" },
     format = function(entry, vim_item)
       -- Kind icons
-      vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+      vim_item.kind = kind_icons[vim_item.kind]
 
       if entry.source.name == "cmp_tabnine" then
-        -- if entry.completion_item.data ~= nil and entry.completion_item.data.detail ~= nil then
-          -- menu = entry.completion_item.data.detail .. " " .. menu
-        -- end
         vim_item.kind = icons.misc.Robot
       end
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- NOTE: order matters
       vim_item.menu = ({
-        -- nvim_lsp = "[LSP]",
-        -- nvim_lua = "[Nvim]",
-        -- luasnip = "[Snippet]",
-        -- buffer = "[Buffer]",
-        -- path = "[Path]",
-        -- emoji = "[Emoji]",
-
         nvim_lsp = "",
         nvim_lua = "",
         luasnip = "",
@@ -122,13 +111,6 @@ cmp.setup {
     native_menu = false,
   },
 }
-
--- cmdline
--- cmp.setup.cmdline(":", {
---     sources = {
---       { name = "cmdline" },
---     },
--- })
 
 -- lsp_document_symbols
 cmp.setup.cmdline('/', {
