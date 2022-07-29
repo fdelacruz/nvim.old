d99a3e756fcfdcf36b467e5cb80a2abe723a87de
Francisco De La Cruz <helloquico@gmail.com>
2022-07-29 12:18:13 -0400

    add browse

---
+++ lua/user/browse.lua
@@ -0,0 +1,43 @@
+local status_ok, browse = pcall(require, "browse")
+if not status_ok then
+  return
+end
+
+local bookmarks = {
+  "https://github.com",
+  "https://github.com/christianchiarulli/nvim",
+  "https://github.com/rockerBOO/awesome-neovim",
+  "https://doc.rust-lang.org/book/",
+  "https://news.ycombinator.com/",
+  "https://github.com/neovim/neovim",
+  "https://neovim.discourse.group/",
+}
+
+local function command(name, rhs, opts)
+  opts = opts or {}
+  vim.api.nvim_create_user_command(name, rhs, opts)
+end
+
+command("BrowseInputSearch", function()
+  browse.input_search()
+end, {})
+
+command("Browse", function()
+  browse.browse { bookmarks = bookmarks }
+end, {})
+
+command("BrowseBookmarks", function()
+  browse.open_bookmarks { bookmarks = bookmarks }
+end, {})
+
+command("BrowseDevdocsSearch", function()
+  browse.devdocs.search()
+end, {})
+
+command("BrowseDevdocsFiletypeSearch", function()
+  browse.devdocs.search_with_filetype()
+end, {})
+
+command("BrowseMdnSearch", function()
+  browse.mdn.search()
+end, {})
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -136,6 +136,7 @@ return packer.startup(function(use)
   use "nvim-telescope/telescope.nvim"
   use "tom-anders/telescope-vim-bookmarks.nvim"
   use "nvim-telescope/telescope-media-files.nvim"
+  use "lalitmee/browse.nvim"
 
   -- Treesitter
   use {
