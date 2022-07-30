e4132cc8b85a367618a56a6f7035a0a0e81c29b0
Francisco De La Cruz <helloquico@gmail.com>
2022-07-30 14:33:33 -0400

    add M-o to open browser bookmarks

--- lua/user/browse.lua
+++ lua/user/browse.lua
@@ -41,3 +41,9 @@ end, {})
 command("BrowseMdnSearch", function()
   browse.mdn.search()
 end, {})
+
+local opts = { noremap = true, silent = true }
+
+local keymap = vim.api.nvim_set_keymap
+
+keymap("n", "<M-o>", "<cmd>BrowseBookmarks<cr>", opts)
