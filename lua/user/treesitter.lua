1bd6f29ed9e80a2f7ab79624013c940dd266b47f
Francisco De La Cruz <helloquico@gmail.com>
2022-07-30 17:52:09 -0400

    update treesitter formating

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -3,34 +3,41 @@ if not status_ok then
   return
 end
 
-configs.setup({
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "" }, -- List of parsers to ignore installing
-    highlight = {
-      -- use_languagetree = true,
-      enable = true, -- false will disable the whole extension
-      disable = { "css" }, -- list of language that will be disabled
-      additional_vim_regex_highlighting = false,
+-- local ft_to_parser = require("nvim-treesitter.parsers").filetype_to_parsername
+-- ft_to_parser.motoko = "typescript"
+
+configs.setup {
+  ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+  ignore_install = { "" }, -- List of parsers to ignore installing
+  highlight = {
+    -- use_languagetree = true,
+    enable = true, -- false will disable the whole extension
+    -- disable = { "css" }, -- list of language that will be disabled
+    -- additional_vim_regex_highlighting = true,
+  },
+  autopairs = {
+    enable = true,
+  },
+  indent = { enable = true, disable = { "python", "css", "rust" } },
+  context_commentstring = {
+    enable = true,
+    enable_autocmd = false,
+  },
+  autotag = {
+    enable = true,
+    disable = { "xml", "markdown" },
+  },
+  rainbow = {
+    enable = false,
+    colors = {
+      "Gold",
+      "Orchid",
+      "DodgerBlue",
+      -- "Cornsilk",
+      -- "Salmon",
+      -- "LawnGreen",
     },
-    autopairs = {
-      enable = true,
-    },
-    indent = { enable = true, disable = { "python", "css", "rust" } },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-    rainbow = {
-      enable = true,
-      colors = {
-        "Gold",
-        "Orchid",
-        "DodgerBlue",
-        -- "Cornsilk",
-        -- "Salmon",
-        -- "LawnGreen",
-      },
-      disable = { "html" },
-    },
-})
+    disable = { "html" },
+  },
+}
