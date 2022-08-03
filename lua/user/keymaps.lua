5e84430d317c62d23d8d78442801692038c935d6
Francisco De La Cruz <helloquico@gmail.com>
2022-08-03 11:09:35 -0400

    see current tabpage in winbar

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -32,6 +32,9 @@ keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
+-- keymap("n", "n", "nzzzv", opts)
+-- keymap("n", "N", "Nzzzv", opts)
+
 -- Navigate buffers
 -- keymap("n", "<S-l>", ":bnext<CR>", opts)
 -- keymap("n", "<S-h>", ":bprevious<CR>", opts)
