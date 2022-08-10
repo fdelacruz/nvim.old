51bc9a1c327f12716c8d5bac1a16e6f47d462e21
Francisco De La Cruz <helloquico@gmail.com>
2022-08-09 23:34:32 -0400

    update keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -94,24 +94,19 @@ keymap(
 )
 keymap("n", "<F11>", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 keymap("n", "<F12>", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-keymap("v", "//", [[y/\V<C-R>=escape(@",'/\')<CR><CR>]], opts)
-keymap(
-  "n",
-  "<C-p>",
-  "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
-  opts
-)
-vim.api.nvim_set_keymap("n", "<M-g>", "<cmd>Telescope git_branches<CR>", opts)
+keymap("v", "//", [[y/\V<c-R>=escape(@",'/\')<CR><CR>]], opts)
+keymap("n", "<C-p>", "<cmd>Telescope projects<cr>", opts)
+vim.api.nvim_set_keymap("n", "<m-g>", "<cmd>Telescope git_branches<cr>", opts)
 keymap("n", "<C-t>", "<cmd>lua vim.lsp.buf.document_symbol()<cr>", opts)
 keymap("n", "<C-s>", "<cmd>vsplit<cr>", opts)
 keymap("n", "<C-z>", "<cmd>ZenMode<cr>", opts)
 keymap("n", "<C-n>", ":e ~/Notes/<cr>", opts)
-keymap("n", "-", ":lua require'lir.float'.toggle()<CR>", opts)
+keymap("n", "-", ":lua require'lir.float'.toggle()<cr>", opts)
 -- keymap("n", "<C-\\>", "<cmd>vsplit<cr>", opts)
--- vim.cmd[[nnoremap c* /\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgn]]
--- vim.cmd[[nnoremap c# ?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]]
--- keymap("n", "c*", [[/\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgn]], opts)
--- keymap("n", "c#", [[?\<<C-R>=expand('<cword>')<CR>\>\C<CR>``cgN]], opts)
+-- vim.cmd[[nnoremap c* /\<<c-R>=expand('<cword>')<CR>\>\c<CR>``cgn]]
+-- vim.cmd[[nnoremap c# ?\<<c-R>=expand('<cword>')<CR>\>\c<CR>``cgN]]
+-- keymap("n", "c*", [[/\<<c-R>=expand('<cword>')<CR>\>\c<CR>``cgn]], opts)
+-- keymap("n", "c#", [[?\<<c-R>=expand('<cword>')<CR>\>\c<CR>``cgN]], opts)
 -- keymap("n", "gx", [[:execute '!brave ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
 keymap("n", "gx", [[:silent execute '!$BROWSER ' . shellescape(expand('<cfile>'), 1)<CR>]], opts)
 -- Change '<CR>' to whatever shortcut you like :)
@@ -140,5 +135,12 @@ vim.api.nvim_set_keymap("n", "K", ":lua require('user.keymaps').show_documentati
 -- Comment
 keymap("n", "<m-/>", "<cmd>lua require('Comment.api').toggle_current_linewise()<CR>", opts)
 keymap("x", "<m-/>", '<ESC><CMD>lua require("Comment.api").toggle_linewise_op(vim.fn.visualmode())<CR>', opts)
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<tab>",
+  "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>",
+  opts
+)
 
 return M
