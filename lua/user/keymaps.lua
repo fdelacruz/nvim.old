74c0abb149a05eb62f5edd1e043a1d8091614897
Francisco De La Cruz <helloquico@gmail.com>
2022-08-12 22:00:40 -0400

    default window navigation

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -22,10 +22,10 @@ keymap("n", "<C-Space>", "<cmd>WhichKey \\<leader><cr>", opts)
 
 -- Normal --
 -- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+-- keymap("n", "<C-h>", "<C-w>h", opts)
+-- keymap("n", "<C-j>", "<C-w>j", opts)
+-- keymap("n", "<C-k>", "<C-w>k", opts)
+-- keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Tabs --
 keymap("n", "<M-t>", ":tabnew<cr>", opts)
