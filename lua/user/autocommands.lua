2894dd203d0d5ea614a558c9e11ade7f2e9c2f3e
Francisco De La Cruz <helloquico@gmail.com>
2022-08-13 22:46:35 -0400

    update autocommands and options

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -152,3 +152,14 @@ vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
     vim.cmd "checktime"
   end,
 })
+
+vim.api.nvim_create_autocmd({ "CursorHold" }, {
+  callback = function()
+    local luasnip = require "luasnip"
+    if luasnip.expand_or_jumpable() then
+      -- ask maintainer for option to make this silent
+      -- luasnip.unlink_current()
+      vim.cmd [[silent! lua require("luasnip").unlink_current()]]
+    end
+  end,
+})
