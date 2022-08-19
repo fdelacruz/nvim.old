5d4f8fd2ab28323ee6ac8bbe5dcf4b3a043f5754
Francisco De La Cruz <helloquico@gmail.com>
2022-08-19 15:57:15 -0400

    cleanup

--- init.lua
+++ init.lua
@@ -55,13 +55,3 @@ require "user.tree-climber"
 require "user.auto-session"
 require "user.jaq"
 require "user.harpoon"
--- revisit this
--- function prequire(package)
---   local status, lib = pcall(require, package)
---   if status then
---     return lib
---   else
---     vim.notify("Failed to require '" .. package .. "' from " .. debug.getinfo(2).source)
---     return nil
---   end
--- end
