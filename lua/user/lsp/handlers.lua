1c1a2950373a4722a97e91d04295ab43a8bb5def
Francisco De La Cruz <helloquico@gmail.com>
2022-08-22 21:36:29 -0400

    update handlers configuration

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -61,16 +61,6 @@ M.setup = function()
   })
 end
 
-local function lsp_highlight_document(client)
-  -- if client.resolved_capabilities.document_highlight then
-    local status_ok, illuminate = pcall(require, "illuminate")
-    if not status_ok then
-      return
-    end
-    illuminate.on_attach(client)
-  -- end
-end
-
 local function attach_navic(client, bufnr)
   vim.g.navic_silence = false
   local status_ok, navic = pcall(require, "nvim-navic")
@@ -103,7 +93,6 @@ end
 M.on_attach = function(client, bufnr)
 
   lsp_keymaps(bufnr)
-  lsp_highlight_document(client)
   attach_navic(client, bufnr)
 
   if client.name == "tsserver" then
