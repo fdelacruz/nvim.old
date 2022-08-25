cbb9e62c89f9ee79b56595d639213ef4755193ef
Francisco De La Cruz <helloquico@gmail.com>
2022-08-25 14:11:46 -0400

    rust dap setup

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -8,7 +8,39 @@ if not dap_ui_status_ok then
   return
 end
 
--- dapui.setup()
+-- rust adapter
+dap.adapters.lldb = {
+  type = "executable",
+  command = "/usr/local/opt/llvm/bin/lldb-vscode", -- adjust as needed, must be absolute path
+  name = "lldb",
+}
+
+dap.configurations.rust = {
+  {
+    name = "Launch",
+    type = "lldb",
+    request = "launch",
+    program = function()
+      return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    end,
+    cwd = "${workspaceFolder}",
+    stopOnEntry = false,
+    args = {},
+
+    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --
+    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --
+    -- Otherwise you might get the following error:
+    --
+    --    Error on launch: Failed to attach to the target process
+    --
+    -- But you should be aware of the implications:
+    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    runInTerminal = false,
+  },
+}
+
 dapui.setup {
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
