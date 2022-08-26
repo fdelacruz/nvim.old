6934657f1ddc5ea6b66c35a3aa6f988ca90e48d0
Francisco De La Cruz <helloquico@gmail.com>
2022-08-26 09:12:18 -0400

    for RustDebuggables

--- lua/user/lsp/settings/rust.lua
+++ lua/user/lsp/settings/rust.lua
@@ -85,42 +85,13 @@ return {
       },
     },
   },
+
+  -- debugging stuff
+  dap = {
+    adapter = {
+      type = "executable",
+      command = "/usr/local/opt/llvm/bin/lldb-vscode",
+      name = "lldb",
+    },
+  },
 }
--- return {
---   settings = {
---     rust_analyzer = {
---       inlayHints = {
---         bindingModeHints = {
---           enable = true,
---         },
---         typeHints = {
---           enable = true,
---           hideClosureInitialization = false,
---           hideNamedConstructor = false,
---         },
---         chainingHints = {
---           enable = true,
---         },
---         closingBraceHints = {
---           enable = true,
---           minLines = 25,
---         },
---         closureReturnTypeHints = {
---           enable = "never",
---         },
---         lifetimeElisionHints = {
---           enable = "never",
---           useParameterNames = false,
---           maxLength = 25,
---         },
---         parameterHints = {
---           enable = true,
---         },
---         reborrowHints = {
---           enable = "never",
---         },
---         renderColons = true,
---       },
---     },
---   },
--- }
