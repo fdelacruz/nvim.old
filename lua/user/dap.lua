170b750b8553ecb7124371e0e05a6f2f754e76a8
Francisco De La Cruz <helloquico@gmail.com>
2022-08-27 08:00:40 -0400

    python dap setup

--- lua/user/dap.lua
+++ lua/user/dap.lua
@@ -41,6 +41,36 @@ dap.configurations.rust = {
   },
 }
 
+-- python adapter
+dap.adapters.python = {
+      type = "executable",
+      command = "/Users/fdelacruz/Workspace/debugpy/bin/python3.9",
+      args = { "-m", "debugpy.adapter" },
+    }
+
+    dap.configurations.python = {
+      {
+        name = "Launch",
+        type = "python",
+        request = "launch",
+        -- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+        program = "${file}", -- This configuration will launch the current file if used.
+        pythonPath = function()
+          -- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+          -- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+          -- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+          local cwd = vim.fn.getcwd()
+          if vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+            return cwd .. "/venv/bin/python"
+          elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+            return cwd .. "/.venv/bin/python"
+          else
+            return "/usr/bin/python3"
+          end
+        end,
+      },
+    }
+
 dapui.setup {
   icons = { expanded = "▾", collapsed = "▸" },
   mappings = {
