8186cde2c400f15d1b4d6d9b415a2dfd66c9f90d
Francisco De La Cruz <helloquico@gmail.com>
2022-12-16 21:54:23 +0100

    update dressing configuration

--- lua/user/dressing.lua
+++ lua/user/dressing.lua
@@ -31,11 +31,6 @@ dressing.setup {
     max_width = { 140, 0.9 },
     min_width = { 20, 0.2 },
 
-    -- Window transparency (0-100)
-    winblend = 10,
-    -- Change default highlight groups (see :help winhl)
-    winhighlight = "",
-
     override = function(conf)
       -- This is the config that will be passed to nvim_open_win.
       -- Change values here to customize the layout
@@ -44,6 +39,12 @@ dressing.setup {
 
     -- see :help dressing_get_config
     get_config = nil,
+    win_options = {
+    -- Change default highlight groups (see :help winhl)
+      winhighlight = "",
+    -- Window transparency (0-100)
+      winblend = 10,
+    }
   },
   select = {
     -- Set to false to disable the vim.ui.select implementation
@@ -89,11 +90,6 @@ dressing.setup {
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "editor",
-
-      -- Window transparency (0-100)
-      winblend = 10,
-      -- Change default highlight groups (see :help winhl)
-      winhighlight = "",
 
       -- These can be integers or a float between 0 and 1 (e.g. 0.4 for 40%)
       -- the min_ and max_ options can be a list of mixed types.
