317a8e7f816e0c94e06e85ccc9410eb0b8849316
Francisco De La Cruz <helloquico@gmail.com>
2022-12-18 20:16:14 +0100

    update nvim-cmp repo

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -110,8 +110,7 @@ return packer.startup(function(use)
   use "rebelot/kanagawa.nvim"
 
   -- cmp plugins
-  -- use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "christianchiarulli/nvim-cmp"
+  use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
