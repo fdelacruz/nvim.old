850b38bb0ec4247dc3fa45458a38e47c5f56a7fe
Francisco De La Cruz <helloquico@gmail.com>
2022-12-20 11:05:18 +0100

    visit super-tab like mapping

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -38,7 +38,8 @@ require("luasnip/loaders/from_vscode").lazy_load()
 --   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
 -- end
 
-local check_backspace = function()
+local has_words_before = function()
+  unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
@@ -84,26 +85,19 @@ cmp.setup {
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
-    -- ["<Right>"] = cmp.mapping.confirm { select = true },
+
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.jumpable(1) then
-        luasnip.jump(1)
       elseif luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif check_backspace() then
-        -- cmp.complete()
-        fallback()
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end, { "i", "s", }),
+
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
@@ -112,10 +106,7 @@ cmp.setup {
       else
         fallback()
       end
-    end, {
-      "i",
-      "s",
-    }),
+    end, { "i", "s", }),
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
