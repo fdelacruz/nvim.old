3954b6268c60e1b47366250993c7d8198c04b7ca
Francisco De La Cruz <helloquico@gmail.com>
2022-12-24 14:58:57 +0100

    cleanup a little

--- lua/user/luasnip.lua
+++ lua/user/luasnip.lua
@@ -1,4 +1,4 @@
-local ls = require("luasnip") --{{{
+local ls = require("luasnip")
 
 -- require("luasnip.loaders.from_vscode").lazy_load()
 require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets/" })
@@ -6,7 +6,7 @@ require("luasnip").config.setup({ store_selection_keys = "<A-p>" })
 
 vim.cmd([[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]]) --}}}
 
--- Virtual Text{{{
+-- Virtual Text
 local types = require("luasnip.util.types")
 ls.config.set_config({
 	history = true, --keep around last snippet local to jump back
@@ -24,45 +24,11 @@ ls.config.set_config({
 		-- 	},
 		-- },
 	},
-}) --}}}
-
--- Key Mapping
+})
 
 -- vim.keymap.set({ "i", "s" }, "<C-u>", '<cmd>lua require("luasnip.extras.select_choice")()<cr><C-c><C-c>')
 
--- vim.keymap.set({ "i", "s" }, "<A-p>", function()
--- 	if ls.expand_or_jumpable() then
--- 		ls.expand()
--- 	end
--- end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<C-k>", function()
--- 	if ls.expand_or_jumpable() then
--- 		ls.expand_or_jump()
--- 	end
--- end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<C-j>", function()
--- 	if ls.jumpable() then
--- 		ls.jump(-1)
--- 	end
--- end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<A-y>", "<Esc>o", { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<A-k>", function()
--- 	if ls.jumpable(1) then
--- 		ls.jump(1)
--- 	end
--- end, { silent = true })
-
--- vim.keymap.set({ "i", "s" }, "<A-j>", function()
--- 	if ls.jumpable(-1) then
--- 		ls.jump(-1)
--- 	end
--- end, { silent = true })
-
-vim.keymap.set({ "i", "s" }, "<A-l>", function()
+vim.keymap.set({ "i", "s" }, "<C-j>", function()
 	if ls.choice_active() then
 		ls.change_choice(1)
 	else
@@ -73,7 +39,7 @@ vim.keymap.set({ "i", "s" }, "<A-l>", function()
 	end
 end)
 
-vim.keymap.set({ "i", "s" }, "<A-h>", function()
+vim.keymap.set({ "i", "s" }, "<C-k>", function()
 	if ls.choice_active() then
 		ls.change_choice(-1)
 	else
