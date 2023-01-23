eb49ba445e36ff7ef679cdf2603147ab953c4df6
Francisco De La Cruz <helloquico@gmail.com>
2023-01-23 22:28:57 +0530

    lazy load neoscroll

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,7 +65,10 @@ return {
       require("twilight").setup {}
     end
   },
-  "karb94/neoscroll.nvim",
+  { "karb94/neoscroll.nvim",
+    keys = { { "<C-u>" }, { "<C-d>" }, { "<C-b>" }, { "<C-f>" }, { "<C-y>" }, { "<C-e>" }, { "zt" }, { "zz" },{  "zb" } },
+    config = true
+  },
   "folke/todo-comments.nvim",
   "andymass/vim-matchup",
   "is0n/jaq-nvim",
