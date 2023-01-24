054407948628fd365b3991c6bf5a11e106020394
Francisco De La Cruz <helloquico@gmail.com>
2023-01-24 15:25:11 +0530

    install the latest stable version

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -13,7 +13,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
 
-  defaults = { lazy = true },
+  defaults = { lazy = true, version = "" },
 
   ui = {
     border = "rounded",
