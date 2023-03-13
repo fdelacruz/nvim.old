872a5391a7422d03d5a22a77b2b80c6930d0b35b
Francisco De La Cruz <helloquico@gmail.com>
2023-03-13 10:23:04 +0530

    update trouble keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -193,4 +193,12 @@ keymap("n", "<Leader>dl", "<Cmd>lua require'dap'.run_last()<CR>", opts)
 keymap("n", "<Leader>du", "<Cmd>lua require'dapui'.toggle()<CR>", opts)
 keymap("n", "<Leader>dt", "<Cmd>lua require'dap'.terminate()<CR>", opts)
 
+-- Trouble
+keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
+keymap("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts)
+keymap("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", opts)
+keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", opts)
+keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", opts)
+keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", opts)
+
 return M
